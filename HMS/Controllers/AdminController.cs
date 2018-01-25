47fd77e35381c4db184b4420d12493d9d27a0033
ahashans <ahashans@gmail.com>
2018-01-25 12:39:05 +0600

    Created Admin Controller And CreateUser Action With View

---
+++ HMS/Controllers/AdminController.cs
@@ -0,0 +1,90 @@
+﻿using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using HMS.Models;
+using HMS.ViewModel;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+
+namespace HMS.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class AdminController : Controller
+    {
+        private ApplicationSignInManager _signInManager;
+        private ApplicationUserManager _userManager;
+
+        public AdminController()
+        {
+        }
+
+        public AdminController(ApplicationUserManager userManager, ApplicationSignInManager signInManager)
+        {
+            UserManager = userManager;
+            SignInManager = signInManager;
+        }
+        public ApplicationSignInManager SignInManager {
+            get {
+                return _signInManager ?? HttpContext.GetOwinContext().Get<ApplicationSignInManager>();
+            }
+            private set {
+                _signInManager = value;
+            }
+        }
+        public ApplicationUserManager UserManager {
+            get {
+                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set {
+                _userManager = value;
+            }
+        }
+
+
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult CreateUser()
+        {
+            return View();
+        }
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> CreateUser(CreateUserViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser { FullName = model.FullName, Gender = model.Gender, DateOfBirth = model.DateOfBirth, Address = model.Address, Mobile = model.Mobile, UserName = model.Email, Email = model.Email };
+                var result = await UserManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    
+                    await UserManager.AddToRoleAsync(user.Id, model.Role);
+                    await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
+
+                    // For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=320771
+                    // Send an email with this link
+                    // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
+                    // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
+                    // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
+
+                    return RedirectToAction("Index", "Home");
+                }
+                AddErrors(result);
+            }
+
+            // If we got this far, something failed, redisplay form
+            return View(model);
+        }
+        private void AddErrors(IdentityResult result)
+        {
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError("", error);
+            }
+        }
+    }
+}
---
+++ HMS/ViewModel/CreateUserViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace HMS.ViewModel
+{
+    public class CreateUserViewModel
+    {
+        [Required(ErrorMessage = "Full Name is Required!")]
+        [Display(Name = "Full Name")]
+        [StringLength(50)]
+        public string FullName { get; set; }
+        [Required(ErrorMessage = "Gender is Required!")]
+        [StringLength(6)]
+        public string Gender { get; set; }
+        [Required(ErrorMessage = "Date Of Birth is Required!")]
+        [Display(Name = "Date Of Birth")]
+        public DateTime? DateOfBirth { get; set; }
+        [Required(ErrorMessage = "Address is Required!")]
+        [StringLength(450)]
+        public string Address { get; set; }
+        [Required(ErrorMessage = "Mobile Number is Required!")]
+        [Display(Name = "Mobile Number")]
+        [StringLength(11)]
+        public string Mobile { get; set; }
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+
+        [Required(ErrorMessage = "Role is Required!")]
+        public string Role { get; set; }
+    }
+}
