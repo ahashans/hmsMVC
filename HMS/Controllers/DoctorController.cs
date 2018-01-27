4a7a623fda5759d835cb67ccd9c89590bbe4622d
ahashans <ahashans@gmail.com>
2018-01-27 13:11:17 +0600

    Added Doctor Controller with Index view

---
+++ HMS/Controllers/DoctorController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace HMS.Controllers
+{
+    public class DoctorController : Controller
+    {
+        // GET: Doctor
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
