4292bd6eede2a3ae754a9ef3025576d4eb54fa5f
ahashans <ahashans@gmail.com>
2018-01-28 11:39:31 +0600

    Completed View Diagnosis Action Of Doctor Controller With View

--- HMS/Controllers/DoctorController.cs
+++ HMS/Controllers/DoctorController.cs
@@ -19,6 +19,45 @@ namespace HMS.Controllers
         {
             return View();
         }
+        public ActionResult ViewDiagnosis()
+        {
+            var doctorId = User.Identity.GetUserId();
+            var patients = from User in _context.Users
+                join Ticket in _context.Tickets on User.Id equals Ticket.PatientUserId
+                join Diagnosis in _context.Diagnoses on Ticket.TicketId equals Diagnosis.TicketId
+                join DoctorDepartment in _context.DoctorDepartments on Ticket.DepartmentId equals DoctorDepartment
+                    .DepartmentId
+                where DoctorDepartment.DoctorUserId == doctorId
+                select User;
+            //var pat = patients.ToList();
+            var viewModel = new DisplayDiagnosisVIewModel
+            {
+                Patients = patients
+            };
+            return View(viewModel);
+        }
+
+        public JsonResult GetDiagnosis(string patientId)
+        {
+            var diagnosis = from diag in _context.Diagnoses
+                join tick in _context.Tickets on diag.TicketId equals tick.TicketId
+                join user in _context.Users on diag.DoctorUserId equals user.Id
+                join dept in _context.Departments on tick.DepartmentId equals dept.DepartmentId
+                where tick.PatientUserId == patientId
+                where tick.TicketStatus == "Closed"
+                select new
+                {
+                    ticketId = tick.TicketId,
+                    departmentName = dept.Name,
+                    diagnosedBy = user.FullName,
+                    diagnosisProvided = diag.DiagnosisProvided,
+                    treatmentProvided = diag.TreatmentProvided,
+                    requiredTest = diag.RequiredTests,
+                    diagnosisDate = diag.DiagnosisTimeStamp
+                };
+
+            return Json(diagnosis, JsonRequestBehavior.AllowGet);
+        }
         public ActionResult CreateDiagnosis()
         {
             var roleId = _context.Roles.Where(m => m.Name == "Patient").Select(m => m.Id).SingleOrDefault();
@@ -38,6 +77,18 @@ namespace HMS.Controllers
             return View(viewModel);
         }
         public JsonResult GetPatientInfo(string patientId)
+        {
+            
+            var patientData = from pat in _context.Users
+                where pat.Id == patientId
+                select new
+                {
+                    dob = pat.DateOfBirth,
+                    gender = pat.Gender
+                };
+            return Json(patientData, JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult GetOpenTicketPatientInfo(string patientId)
         {
             var doctorId = User.Identity.GetUserId();
             var patientData = from pat in _context.Users
---
+++ HMS/ViewModel/DisplayDiagnosisVIewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using HMS.Models;
+
+namespace HMS.ViewModel
+{
+    public class DisplayDiagnosisVIewModel
+    {
+        public IEnumerable<ApplicationUser> Patients { get; set; }
+
+
+    }
+}
